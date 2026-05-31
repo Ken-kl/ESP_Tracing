701c400dbd2340052d495a9af85e043c960f9ca4
Ken <uhjmj@student.kit.edu>
2026-05-31 16:48:01 +0200

    removed unnecessary files, upadted git ignore

--- main/esp32_tracing.c
+++ main/esp32_tracing.c
@@ -1,8 +1,11 @@
+#include "driver/gpio.h"
 #include "esp_log.h"
 #include "soc/soc.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "esp_attr.h"
+
+#define BLINK_GPIO GPIO_NUM_8
 #define DR_REG_PCR_BASE 0x60096000
 #define TRACE_BASE_ADDRESS 0x600C0000
 
@@ -23,7 +26,7 @@ void app_main(void)
 {
     // Clear memory address
     memset(trace_buffer, 0, TRACE_BUFFER_SIZE);
-    ESP_LOGI("BUFFER","Resetting trace buffer");
+    ESP_LOGI("BUFFER", "Resetting trace buffer");
     uint32_t start_address = (uint32_t)&trace_buffer[0];
     uint32_t end_address = start_address + TRACE_BUFFER_SIZE - 4;
     ESP_LOGI("TRACE", "Buffer allocated at: 0x%08X - 0x%08X", start_address, end_address);
@@ -46,8 +49,19 @@ void app_main(void)
     asm volatile("nop; nop; nop; nop;");
     ESP_LOGI("TRACE", "Hardware should be streaming now.");
 
+    gpio_reset_pin(BLINK_GPIO);
+    gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT);
+
     while (1)
     {
+        ESP_LOGI("LED", "Turning LED ON");
+        gpio_set_level(BLINK_GPIO, 1);
+        vTaskDelay(pdMS_TO_TICKS(1000));
+
+        ESP_LOGI("LED", "Turning LED OFF");
+        gpio_set_level(BLINK_GPIO, 0);
+        vTaskDelay(pdMS_TO_TICKS(1000));
+
         /*REG_WRITE(TRACE_TRIGGER_REG, 0x6);
         __asm__ __volatile__("nop");
         uint32_t current = *(volatile uint32_t *)TRACE_MEM_CURRENT_ADDR_REG;
@@ -58,7 +72,5 @@ void app_main(void)
             ESP_LOGW("TRACE", "Buffer is almost full!");
         }
         REG_WRITE(TRACE_TRIGGER_REG, 0x5);*/
-
-        vTaskDelay(pdMS_TO_TICKS(500));
     }
 }
